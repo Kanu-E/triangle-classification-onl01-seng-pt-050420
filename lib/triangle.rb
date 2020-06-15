85817551e6b7fe5152cd757d57d9bc9ee9a4be1c
Emeka Kanu <chukwuemekakanu@gmail.com>
2020-06-15 20:04:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,11 @@
 class Triangle
  
  def initialize (side_a, side_b, side_c)
-   @side_a = side_a
-   @side_b = side_b
-   @side_c = side_c
+   @side_a, @side_b, @side_c = side_a, side_b, side_c
  end 
  
  def kind
-   if (side_a == side_b) && (side_b == side_c)
+   if @side_a == @side_b && @side_b == @side_c
       :equilateral
    end
  end
