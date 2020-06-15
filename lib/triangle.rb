393aa78b061e54ff8e30232ac71e38750ab9f593
Emeka Kanu <chukwuemekakanu@gmail.com>
2020-06-15 20:56:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,18 +8,18 @@ class Triangle
    if ((@side_a <= 0) || (@side_b <= 0) || (@side_c <= 0)) || ((@side_c + @side_a <= @side_b) || (@side_b + @side_a <= @side_c) || (@side_c + @side_b <= @side_a))
      raise TriangleError
     else
-   if @side_a == @side_b && @side_b == @side_c
+    if @side_a == @side_b && @side_b == @side_c
       :equilateral
-      elsif (@side_a == @side_b) ||  ( @side_b == @side_c) || (@side_a == @side_c)
+    elsif @side_a == @side_b || @side_b == @side_c || @side_a == @side_c
       :isosceles
     elsif @side_a != @side_b && @side_b != @side_c && @side_a != @side_c
-      :scalene
+     :scalene
+    end
    end
- end
- end
+  end
  class TriangleError < StandardError
   
-end
+ lend
 end
 
 
