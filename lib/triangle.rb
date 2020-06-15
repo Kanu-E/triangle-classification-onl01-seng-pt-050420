5dc45dac05b68096dfe65ce9933fa51eb10bd9a5
Emeka Kanu <chukwuemekakanu@gmail.com>
2020-06-15 20:13:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
  def kind
    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
-    else
+    end
    if @side_a == @side_b && @side_b == @side_c
       :equilateral
       elsif (@side_a == @side_b && @side_b != @side_c) ||  (@side_a != @side_b && @side_b == @side_c) || (@side_a == @side_c && @side_b != @side_c)
