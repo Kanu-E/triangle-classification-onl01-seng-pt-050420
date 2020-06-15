ec47297176dcac34257e8151e8de5a5a5c5b1565
Emeka Kanu <chukwuemekakanu@gmail.com>
2020-06-15 20:09:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
    if @side_a == @side_b && @side_b == @side_c
       :equilateral
       elsif (@side_a == @side_b && @side_b != @side_c) ||  (@side_a != @side_b && @side_b == @side_c) || (@side_a == @side_c && @side_b != @side_c)
-      isosceles
+      :isosceles
    end
  end
 end
